b638cdee4db00e6b913f82aec88cb2a2d84c1ae0
Corey Mastrianni <mastrianni11@gmail.com>
2026-01-29 02:38:12 -0500

    Fix redirect loop - remove old LO-Website paths and update 404 redirect

--- src/App.jsx
+++ src/App.jsx
@@ -54,44 +54,24 @@ function App() {
             <main id="main-content" className="flex-grow">
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/LO-Website/" element={<Home />} />
-                <Route path="/LO-Website/about-us" element={<AboutUs />} />
                 <Route path="/about-us" element={<AboutUs />} />
-                <Route path="/LO-Website/about-dr-gatson" element={<AboutDrGatson />} />
                 <Route path="/about-dr-gatson" element={<AboutDrGatson />} />
-                <Route path="/LO-Website/brainstorm-cancer" element={<BrainStormCancer />} />
                 <Route path="/brainstorm-cancer" element={<BrainStormCancer />} />
-                <Route path="/LO-Website/brainstorm-cancer/:eventId" element={<EventDetail />} />
                 <Route path="/brainstorm-cancer/:eventId" element={<EventDetail />} />
-                <Route path="/LO-Website/oncology-conversations" element={<OncologyConversations />} />
                 <Route path="/oncology-conversations" element={<OncologyConversations />} />
-                <Route path="/LO-Website/educational-hub" element={<EducationalHub />} />
                 <Route path="/educational-hub" element={<EducationalHub />} />
-                <Route path="/LO-Website/testimonials" element={<Testimonials />} />
                 <Route path="/testimonials" element={<Testimonials />} />
-                <Route path="/LO-Website/photo-gallery" element={<PhotoGallery />} />
                 <Route path="/photo-gallery" element={<PhotoGallery />} />
-                <Route path="/LO-Website/contact" element={<Contact />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/LO-Website/services" element={<Services />} />
                 <Route path="/services" element={<Services />} />
-                <Route path="/LO-Website/resources" element={<Resources />} />
                 <Route path="/resources" element={<Resources />} />
-                <Route path="/LO-Website/community" element={<Community />} />
                 <Route path="/community" element={<Community />} />
-                <Route path="/LO-Website/members-directory" element={<MembersDirectory />} />
                 <Route path="/members-directory" element={<MembersDirectory />} />
-                <Route path="/LO-Website/donate" element={<Donate />} />
                 <Route path="/donate" element={<Donate />} />
-                <Route path="/LO-Website/signup" element={<SignUp />} />
                 <Route path="/signup" element={<SignUp />} />
-                <Route path="/LO-Website/login" element={<Login />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/LO-Website/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
                 <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-                <Route path="/LO-Website/welcome" element={<ProtectedRoute><WelcomeDashboard /></ProtectedRoute>} />
                 <Route path="/welcome" element={<ProtectedRoute><WelcomeDashboard /></ProtectedRoute>} />
-                <Route path="/LO-Website/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
                 <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
               </Routes>
             </main>
